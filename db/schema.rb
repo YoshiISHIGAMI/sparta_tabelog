9aa94c40aceaca9519eafd246bc703d3ab8f85ca
Yoshi Ishigami <yoshi@ibf.sc>
2015-06-27 16:30:21 +0900

    User に ユーザー名を追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627065115) do
+ActiveRecord::Schema.define(version: 20150627071213) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150627065115) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "full_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
